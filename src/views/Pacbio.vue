af7e062a8ad644d3fe4cdc258becf97dde47f41c
Harriet Craven <hc6@sanger.ac.uk>
2020-04-21 09:22:02 +0100

    Started to play with apollo

--- src/views/Pacbio.vue
+++ src/views/Pacbio.vue
@@ -1,17 +1,29 @@
 <template>
-  <pipeline-view></pipeline-view>
+  <!-- <pipeline-view></pipeline-view> -->
+  <apollo-example></apollo-example>
 </template>
 
 <script>
 
 import PipelineView from '@/views/PipelineView'
+import ApolloExample from '@/components/ApolloExample'
+import gql from 'graphql-tag'
 
 export default {
   name: 'Pacbio',
   components: {
-    PipelineView
+    PipelineView,
+    ApolloExample
+  },
+  apollo: {
+  // Simple query that will update the 'hello' vue property
+    // hello: gql`query {
+    //   hello
+    // }`,
   },
   created () {
+    debugger
+    console.log(this.$apollo.provider.defaultClient)
     localStorage.setItem('pipeline', 'pacbio')
   }
 }
